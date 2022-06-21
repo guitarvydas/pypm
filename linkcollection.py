209e6710dd5bcf4d6e024b4c826fa63a77cbf637
Paul Tarvydas <guitarvydas@gmail.com>
2022-06-21 16:09:23 -0400

    link collection light test

---
+++ linkcollection.py
@@ -0,0 +1,42 @@
+import leaf
+import re
+import os
+
+class LinkCollection (leaf.Leaf):
+    def __init__ (self, parent, name): 
+        super().__init__ (parent, name)
+        self.links = []
+        self.state = 'idle'
+    def handler (self, port, data):
+        super ().handler (port, data)
+        if (self.state == 'idle'):
+            if (port == '[append list]'):
+                # data is a list of links, each item a string of the form '[[abc]]'
+                self.links = self.links + data
+                self.state = 'appending'
+            else:
+                raise Exception (f'unrecognized message in state idle /{port}/')
+        elif (self.state == 'appending'):
+            if (port == '[append list]'):
+                self.links = self.links + data
+            elif (port == 'req next'):
+                if (0 >= len (self.links)):
+                    self.send ('no more', True)
+                    self.state = 'idle'
+                else:
+                    link = self.links.pop (0)
+                    self.send ('link', link)
+            else:
+                raise Exception (f'unrecognized message in state appending /{port}/')
+            
+def testLinkCollection ():
+    tester = LinkCollection (None, 'link collection')
+    links = ['[[abc]]', '[[def]]']
+    tester.handler ('[append list]', links)
+    tester.handler ('req next', True)
+    tester.handler ('req next', True)
+    link = tester.outputs2dict ()['link']
+    print (link)
+
+testLinkCollection ()
+
