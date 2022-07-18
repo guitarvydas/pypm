6db4732ac418c39757fb32a5b25eebda91eb9d14
Paul Tarvydas <guitarvydas@gmail.com>
2022-07-18 09:57:57 -0400

    grouping raises no errors

--- poc/perfilelinkscraper.py
+++ poc/perfilelinkscraper.py
@@ -5,7 +5,6 @@
 import mpos
 import dispatcher
 import re
-import md__file__reader
 class _perfilelinkscraper (mpos.Container):
   def __init__ (self, dispatcher, parent, idInParent):
     super ().__init__ (dispatcher, parent, idInParent)
@@ -13,10 +12,8 @@ class _perfilelinkscraper (mpos.Container):
     self.outputs=['[links]']
     
     
-    child0 = md__file__reader._md__file__reader (dispatcher, self, 'MD File Reader')        
-    conn0 = mpos.Connector ([mpos.Sender ('', 'filename')], [mpos.Receiver ('MD File Reader', 'filename')])
-    self.connections = [ conn0 ]
-    self.children = {'MD File Reader':child0}
+    self.connections = [  ]
+    self.children = {}
   
   
 
