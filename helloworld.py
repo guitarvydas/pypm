f1ca70a2822e34d62c073e18b2858ec8eaba85e4
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-15 16:21:48 -0400

    hello world hello

--- helloworld.py
+++ helloworld.py
@@ -6,6 +6,7 @@ from world import World
 class HelloWorld (PipelineComponent):
     def __init__ (self, parent, name, data):
         step1 = { 'clss' : Hello, 'name' : f'h[{name}]' }
+        step1a = { 'clss' : Hello, 'name' : f'h[{name}]' }
         step2 = { 'clss': World, 'name' : f'w[{name}]' }
-        pipeline = [step1, step2]
+        pipeline = [step1, step2, step1a]
         super ().__init__ (parent, name, data, pipeline)
