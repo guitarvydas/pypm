146907f97cb88670c17bd8f94f2662089ac4df94
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-15 22:36:43 -0400

    edit instance data for helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ from world import World
 class HelloWorld (PipelineComponent):
     def __init__ (self, parent, name, data):
         step1 = { 'clss' : Hello, 'name' : f'h[{name}]', 'instanceData' : 'a' }
-        step1a = { 'clss' : Hello, 'name' : f'h[{name}]','instanceData' : 'b' }
+        step1a = { 'clss' : Hello, 'name' : f'h[{name}]','instanceData' : 'a1' }
         step2 = { 'clss': World, 'name' : f'w[{name}]'  ,'instanceData' : 'c' }
         pipeline = [step1, step2, step1a]
         super ().__init__ (parent, name, data, pipeline)
